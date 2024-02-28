082e53da7933502b838d5d31b37ac54e70493384
Paul Vincent Cabunilas <paulcabunilas@gmail.com>
2024-02-28 17:32:48 +0100

    Added Swagger documentation

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # packaging-service
 This is a temporary project, To be deleted after 2024-03-31
+
+
+## How to run
+#### Note: You might need to modify gradle wrapper permission to execute the script
+`chmod +x gradlew`
+
+`./gradlew bootRun`
+
+## Api Documentation
+`http://localhost:8080/swagger-ui/webjars/swagger-ui/index.html`
--- src/main/java/com/globe/gcash/packaging/controller/PackageController.java
+++ src/main/java/com/globe/gcash/packaging/controller/PackageController.java
@@ -2,9 +2,13 @@ package com.globe.gcash.packaging.controller;
 
 import com.globe.gcash.packaging.exception.RequestValidationException;
 import com.globe.gcash.packaging.model.PackageInfo;
+import com.globe.gcash.packaging.model.response.GenericErrorResponse;
 import com.globe.gcash.packaging.model.response.PackageCostResponse;
 import com.globe.gcash.packaging.service.PackageService;
-import jakarta.validation.Valid;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,10 +23,21 @@ import reactor.core.publisher.Mono;
 public class PackageController {
     private final PackageService packageService;
 
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Ok", content =
+                    {@Content(mediaType = "application/json", schema =
+                    @Schema(implementation = PackageInfo.class))}),
+            @ApiResponse(responseCode = "400", description = "Invalid Request parameters", content =
+                    {@Content(mediaType = "application/json", schema =
+                    @Schema(implementation = GenericErrorResponse.class))}),
+            @ApiResponse(responseCode = "500", description = "Internal server error", content =
+                    {@Content(mediaType = "application/json", schema =
+                    @Schema(implementation = GenericErrorResponse.class))})})
+
     @GetMapping(path = "/cost")
     public Mono<PackageCostResponse> getCostEstimate(@Validated PackageInfo packageInfo,
                                                      @RequestParam(value = "voucher", required = false) String voucher) {
-        if(!packageInfo.isValid()) { //Simple check for volume, if parameters for L W H is not present, default value is 0.0
+        if (!packageInfo.isValid()) { //Simple check for volume, if parameters for L W H is not present, default value is 0.0
             throw new RequestValidationException("Invalid request, please refer to API documentation.");
         }
         return packageService.calculatePackageCost(packageInfo, voucher);
