efeb080ada529d1209c97883daf29a117ca038af
Paul Vincent Cabunilas <paulcabunilas@gmail.com>
2024-02-28 16:59:52 +0100

    [Initial Commit]

--- src/test/java/com/globe/gcash/packaging/service/PackageServiceTest.java
+++ src/test/java/com/globe/gcash/packaging/service/PackageServiceTest.java
@@ -1,7 +1,7 @@
 package com.globe.gcash.packaging.service;
 
 /***
- * There is no need to create UNIT for package Service since all the method using webclient.
+ * There is no need to create UNIT for package Service since all the method is using webclient.
  * All the test-case and exception handling is covered in PackageControllerTest.
  */
 class PackageServiceTest {
